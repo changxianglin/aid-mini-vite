d02fbc6f8039b0ebe0ccf714fbfb1ae7beaf8916
changxianglin <281532612@qq.com>
2021-04-07 14:39:54 +0800

    basic finish
    finish

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
+  <div>
+    <p>{{count}}</p>
+    <button @click="handleAdd">add</button>
+  </div>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+// import HelloWorld from './components/HelloWorld.vue';
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+
+const count = ref(0);
+const handleAdd = () => {
+  count.value++;
+};
 </script>
 
 <style>
